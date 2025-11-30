28c4b6cae929c1ddc2f0f95e3275f83780133554
azizbaba237 <abdoulazizbaba227@gmail.com>
2025-11-30 12:12:52 +0100

    backend mis a jour

--- turkish_backend/settings.py
+++ turkish_backend/settings.py
@@ -143,19 +143,6 @@ else:
          }
      }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': config("DB_NAME"),
-#        'USER': config("DB_USER"),
-#        'PASSWORD': config("DB_PASSWORD", default=""),
-#        'HOST': config("DB_HOST", default="127.0.0.1"),
-#        'PORT': config("DB_PORT", default="3306"),
-#        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
-#    }
-#}
-
-
 # ----------------------
 # PASSWORDS
 # ----------------------
